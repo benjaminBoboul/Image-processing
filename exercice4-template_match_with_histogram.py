bec57a15586f852acd382b461f9a2b723dc0ac3f
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2019-10-25 16:32:51 +0200

    :package: Refactor histogram template matching
    Added :
    
    *  scale factor and correlaction min to increase/decrease precision.
    * Using pixel instead of shape to browse the picture
    * Possibility to elect multiple candidates with inline array
    *  Empty file for KNN-Implementation
    
    Removed :
    * `x_steps` and `y_steps` variables

--- exercice4-template_match_with_histogram.py
+++ exercice4-template_match_with_histogram.py
@@ -3,39 +3,38 @@ import cv2
 import numpy as np
 import math
 import pprint
+import random
+
+# Increase value to decrease precision and reduce execution time (min : 1) WARN : a scan_scale_factor too high could affect the algorithm's ability to work properly
+scan_scale_factor = 1
+# This variables define the minimum value required to mark position as candidate with HISTCMP_CORREL method, TIPS : set -1 to produce glitchy picture (recommended value : 0.9)
+correlation_minimum = 0.9
 
 img_rgb = cv2.imread("Data-TP/mon.jpg")
 template_rgb = cv2.imread('Data-TP/statue.jpg')
 img_size = img_rgb.shape[:2]
 tmp_size = template_rgb.shape[:2]
-y_steps = range(0, math.floor(img_size[0]/tmp_size[0]))
-x_steps = range(0, math.floor(img_size[1]/tmp_size[1]))
-
-"""
-Issue with border : this doesn't process border if images shape doesn't correlate.
-This will cause the script to ignore right and lower borders
-
-Todo : add mask or resize image to fix this mess
-"""
 
 template_hist = cv2.calcHist(template_rgb, [2], None, [256], [0, 256]) # get template histogram
-hist_cmp = {}
-for y in y_steps:
-    y_range = (tmp_size[0]*y, tmp_size[0]*y+tmp_size[0])
-    for x in x_steps:
-        x_range = (tmp_size[0]*x, tmp_size[0]*x+tmp_size[0])
-        a = img_rgb[y_range[0]:y_range[1], x_range[0]:x_range[1]]
-        #cv2.imshow("current_frame_{}_{}".format(y, x), a) # toggle this line if you want spaghetti windows
-        img_hist = cv2.calcHist(a, [2], None, [256], [0, 256]) # get img_rgb histogram
-        hist_cmp["{}_{}".format(y_range, x_range)] = cv2.compareHist(template_hist, img_hist, cv2.HISTCMP_CORREL)
+hist_cmp = []
+
+for y in range(0, img_size[0] - tmp_size[0], scan_scale_factor):
+    y_upper, y_lower = (y, y+tmp_size[0])
+    for x in range(0, img_size[1] - tmp_size[1], scan_scale_factor):
+        x_upper, x_lower = (x, x+tmp_size[1])
+        print("scanning from {}x{} to {}x{}".format(x_upper, y_upper, x_lower, y_lower))
+        a = img_rgb[y_upper:y_lower, x_upper:x_lower]
+        img_hist = cv2.calcHist(a, [2], None, [256], [0, 256])
+        compared_hist = cv2.compareHist(template_hist, img_hist, cv2.HISTCMP_CORREL)
+        hist_cmp.append((compared_hist, (x_upper, y_upper), (x_lower, y_lower)))
 
 pprint.pprint(hist_cmp)
-print(max(hist_cmp.values()))
+candidates = [x for x in hist_cmp if x[0] >= correlation_minimum] #max(hist_cmp, key=lambda x: x[0])
+print(candidates)
 
-scope = list(hist_cmp.keys())[list(hist_cmp.values()).index(max(hist_cmp.values()))]
-print(scope) # correlation coordinates
-cv2.rectangle(img_rgb, (89, 0), (178, 89), (0, 255, 0), 2) # ok so logs outputs use the following format : (upper y, upper x)_(lower y, lower_x) while rectangle take (upper x, upper y), (lower x, lower y) in input
-# need to find a way to store matches with their localisation in a proper format
+for scope in candidates:
+    cv2.rectangle(img_rgb, scope[1], scope[2], (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)), 2)
+
 cv2.imshow('result', img_rgb)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
